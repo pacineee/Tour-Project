6cdbd79d46da267116ef3f45a57b3f101d1361b9
pacine <pacifiquenezerwa@gmail.com>
2022-09-29 21:42:35 +0200

    Sept

---
+++ myindex.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pacine Tour</title>
+
+    <!--swipper css link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    <!--Font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!--Custom (mine) css file link-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!--header section starts-->
+    <section class="header">
+      <a href="myindex.php" class="logo">Pacine</a>
+      <nav class="navbar">
+        <a href="myindex.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+      </section>
+
+    <!--header section ends-->
+    
+
+
+
+
+
+
+    <!--Swipper js file link-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+    <!--Custom JS file link-->
+    <script src="script.js"></script>
+</body>
+</html>
