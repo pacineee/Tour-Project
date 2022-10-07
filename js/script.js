a525c7d8bc3870d62d049616558ccd43112eea86
pacine <pacifiquenezerwa@gmail.com>
2022-10-07 13:48:23 +0200

    Swipper Js Done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 };
-window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
 
 var swiper = new Swiper(".home-slider", {
-    loop:true,
+    loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
--- myindex.php
+++ myindex.php
@@ -1,163 +1,184 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pacine Tour</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pacine Tour</title>
 
-    <!--swipper css link-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
-    <!--Font awesome cdn link-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <!--Custom (mine) css file link-->
-    <link rel="stylesheet" href="css/style.css">
+  <!--swipper css link-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+  <!--Font awesome cdn link-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+  <!--Custom (mine) css file link-->
+  <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <!--header section starts-->
-    <section class="header">
-      <a href="myindex.php" class="logo">Pacine</a>
-      <nav class="navbar">
-        <a href="myindex.php">home</a>
-        <a href="about.php">about</a>
-        <a href="package.php">publications</a>
-        <a href="book.php">book</a>
-      </nav>
+  <!--header section starts-->
+  <section class="header">
+    <a href="myindex.php" class="logo">Pacine</a>
+    <nav class="navbar">
+      <a href="myindex.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">publications</a>
+      <a href="book.php">book</a>
+    </nav>
 
-      <div id="menu-btn" class="fas fa-bars"></div>
+    <div id="menu-btn" class="fas fa-bars"></div>
 
-      </section>
+  </section>
 
-    <!--header section ends here-->
+  <!--header section ends here-->
 
 
-<!-- Home section starts here -->
-<section class="home">
-  <div class="swiper home-slider">
-    <div class="swiper-wrapper">
+  <!-- Home section starts here -->
+  <section class="home">
 
-      <div class="swiper-slide slide" style="background:url(images/green-fields-with-wild-trees-2210x1473.jpg) no-repeat">
-        <div class="content">
-          <span>Explore, discover, travel</span>
-          <h3>Travel around Rwanda</h3>
-          <a href="package.php" class="btn">Discover more </a>
+    <!-- Swipper -->
+    <div class="swiper home-slider">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide slide" style="background:url(images/green-fields-with-wild-trees-2210x1473.jpg) no-repeat">
+          <div class="content">
+            <span>Explore, discover, travel</span>
+            <h3>Travel around Rwanda</h3>
+            <a href="package.php" class="btn">Discover more </a>
+          </div>
         </div>
-      </div>
 
         <div class="swiper-slide slide" style="background:url(images/forestly_3200-1800.jpg) no-repeat">
-        <div class="content">
-          <span>Explore, discover, travel</span>
-          <h3>Dicover new Amazing Rwandan Places</h3>
-          <a href="package.php" class="btn">Discover more </a>
-        </div>
+          <div class="content">
+            <span>Explore, discover, travel</span>
+            <h3>Discover new Amazing Rwandan Places</h3>
+            <a href="package.php" class="btn">Discover more </a>
+          </div>
         </div>
 
         <div class="swiper-slide slide" style="background:url(images/wp4088639_1900-1200.jpg) no-repeat">
-        <div class="content">
-          <span>Explore, discover, travel</span>
-          <h3>Make your Life count</h3>
-          <a href="package.php" class="btn">Discover more </a>
+          <div class="content">
+            <span>Explore, discover, travel</span>
+            <h3>Make your Life count</h3>
+            <a href="package.php" class="btn">Discover more </a>
+          </div>
         </div>
+
+        <div class="swiper-slide slide" style="background:url(images/wp4088639_1900-1200.jpg) no-repeat">
+          <div class="content">
+            <span>Explore, discover, travel</span>
+            <h3>Make your Life count</h3>
+            <a href="package.php" class="btn">Discover more </a>
+          </div>
         </div>
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+    </div>
+    <!-- Swipper -->
+
+  </section>
+  <!-- Home section ends here -->
+
+
+
+  <!-- service section starts -->
+  <section class="services">
+    <h1 class="heading-title">Our services</h1>
+    <div class="box-container">
+
+      <div class="box">
+        <img src="images/live_fire.png" alt="Oops...smashed">
+        <h3>Advanture</h3>
+      </div>
+      <div class="box">
+        <img src="images/icons8-camp-64.png" alt="Oops...smashed">
+        <h3>Tour guide</h3>
+      </div>
+      <div class="box">
+        <img src="" alt="Oops...smashed">
+        <h3>Trekking</h3>
+      </div>
+      <div class="box">
+        <img src="" alt="Oops...smashed">
+        <h3>Tour guide</h3>
+      </div>
+      <div class="box">
+        <img src="" alt="Oops...smashed">
+        <h3>Camp fire</h3>
+      </div>
+      <div class="box">
+        <img src="" alt="Oops...smashed">
+        <h3>Advanture</h3>
+      </div>
+      <div class="box">
+        <img src="images/icons8-camp-64.png" alt="Oops...smashed">
+        <h3>Camping</h3>
+      </div>
     </div>
 
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-
-  </div>
-</section>
-<!-- Home section ends here -->
-
-
-
-<!-- service section starts -->
-<section class="services">
-  <h1 class="heading-title">Our services</h1>
-<div class="box-container">
-
-  <div class="box">
-    <img src="images/live_fire.png" alt="Oops...smashed">
-    <h3>Advanture</h3>
-  </div>
-  <div class="box">
-    <img src="images/icons8-camp-64.png" alt="Oops...smashed">
-    <h3>Tour guide</h3>
-  </div>
-  <div class="box">
-    <img src="" alt="Oops...smashed">
-    <h3>Trekking</h3>
-  </div>
-  <div class="box">
-    <img src="" alt="Oops...smashed">
-    <h3>Tour guide</h3>
-  </div>
-  <div class="box">
-    <img src="" alt="Oops...smashed">
-    <h3>Camp fire</h3>
-  </div>
-  <div class="box">
-    <img src="" alt="Oops...smashed">
-    <h3>Advanture</h3>
-  </div>
-  <div class="box">
-    <img src="images/icons8-camp-64.png" alt="Oops...smashed">
-    <h3>Camping</h3>
-  </div>
-</div>
-
-</section>
-<!-- service section ends -->
+  </section>
+  <!-- service section ends -->
 
 
 
 
-<!-- Footer section starts -->
-<section class="footer">
-  <div class="box-container">
-    <div class="box">
-      <h3>Quick Links</h3>
-    <a href="myindex.php"><i class="fas fa-angle-right"></i> home</a>
+  <!-- Footer section starts -->
+  <section class="footer">
+    <div class="box-container">
+      <div class="box">
+        <h3>Quick Links</h3>
+        <a href="myindex.php"><i class="fas fa-angle-right"></i> home</a>
         <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
         <a href="package.php"><i class="fas fa-angle-right"></i> publications</a>
         <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+        <h3>Extra Links</h3>
+        <a href="#"><i class="fas fa-angle-right"></i> Ask Questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i> About Us</a>
+        <a href="#"><i class="fas fa-angle-right"></i> Privacy Policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i> Terms of use</a>
+      </div>
+
+      <div class="box">
+        <h3>Extra Links</h3>
+        <a href="#"><i class="fas fa-phone"></i> +250787746988</a>
+        <a href="#"><i class="fab fa-twitter"></i> @PNezerwa</a>
+        <a href="#"><i class="fas fa-envelope"></i> pacineee@gmail.com</a>
+        <a href="#"><i class="fas fa-map"></i> Kicukiro-Kigali-Rwanda</a>
+      </div>
+
+      <div class="box">
+        <h3>Follow Me</h3>
+        <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+        <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+        <a href="#"><i class="fab fa-linkedin"></i> LinkedIn</a>
+      </div>
+
     </div>
 
-    <div class="box">
-      <h3>Extra Links</h3>
-    <a href="#"><i class="fas fa-angle-right"></i> Ask Questions</a>
-    <a href="#"><i class="fas fa-angle-right"></i> About Us</a>
-    <a href="#"><i class="fas fa-angle-right"></i> Privacy Policy</a>
-    <a href="#"><i class="fas fa-angle-right"></i> Terms of use</a>
-    </div>
+    <div class="credit">Developed by <span><a href="#pacine">Pacine</a></span> | All rights reserved...!</div>
 
-    <div class="box">
-      <h3>Extra Links</h3>
-    <a href="#"><i class="fas fa-phone"></i> +250787746988</a>
-    <a href="#"><i class="fab fa-twitter"></i> @PNezerwa</a>
-    <a href="#"><i class="fas fa-envelope"></i> pacineee@gmail.com</a>
-    <a href="#"><i class="fas fa-map"></i> Kicukiro-Kigali-Rwanda</a>
-    </div>
-
-    <div class="box">
-      <h3>Follow Me</h3>
-    <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
-    <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
-    <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
-    <a href="#"><i class="fab fa-linkedin"></i> LinkedIn</a>
-    </div>
-
-  </div>
-
-  <div class="credit">Developed by <span><a href="#pacine">Pacine</a></span> | All rights reserved...!</div>
-
-</section>
-<!-- Footer section ends -->
+  </section>
+  <!-- Footer section ends -->
 
 
-    <!--Swipper js file link-->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+  <!--Swipper js file link-->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
 
-    <!--Custom JS file link-->
-    <script src="script.js"></script>
+
+<!-- Swiper JS 
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+
+  <!--Custom JS file link-->
+  <script src="js/script.js"></script>
 </body>
+
+
 </html>
